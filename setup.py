d2df104f8fd266c96648c9feeff5ce9d3e4bca69
x64bit <74537225+0xbitx@users.noreply.github.com>
2023-09-16 03:05:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ os.system('curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/mas
 os.system('chmod +x betterdiscordctl')
 os.system('sudo mv betterdiscordctl /usr/local/bin')
 os.system('betterdiscordctl install')
+os.system('wget https://gist.githubusercontent.com/0xbitx/201fb6e02258271c452731a77dc8bafb/raw/a1ad565c6381437a2e2a5f08bb37655e29c455f5/dedsec.theme.css')
 time.sleep(1)
 if os.path.exists(path):
     os.system(f'mv dedsec.theme.css "{path}"')
